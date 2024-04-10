97889b3a7676b33093626d5cc90cc910272ca2fb
gakuto45 <gakuto0122@gmail.com>
2024-04-10 18:30:59 +0900

    開発：【メインソフト】起動画面を実装完了

--- dev/RunningVision/src/main.cpp
+++ dev/RunningVision/src/main.cpp
@@ -1,35 +1,28 @@
 #include <M5Unified.h>
 #include <M5GFX.h>
 #include <M5UnitGLASS2.h>
+#include <U8g2lib.h>
 
 HardwareSerial GPSRaw(2);
 //M5UnitGLASS2 display(26,32,40000); // SDA, SCL, FREQ(ATOM ECHO)
-M5UnitGLASS2 display(2,1,40000); // SDA, SCL, FREQ(ATOM ECHO)
-
+M5UnitGLASS2 display(2,1,40000); // SDA, SCL, FREQ(ATOM S3)
 M5Canvas canvas(&display);
+U8G2_SSD1309_128X64_NONAME0_F_HW_I2C u8g2(U8G2_R2, /* clock=*/ 1, /* data=*/ 2, /* reset=*/ U8X8_PIN_NONE); //* clock=*/ SCL, /* data=*/ SDA
 
-static constexpr char text0[] = "hello user";
-static constexpr char text1[] = "this";
-static constexpr char text2[] = "is";
-static constexpr char text3[] = "RunningVision";
-static constexpr char text4[] = "support";
-static constexpr char text5[] = "Your";
-static constexpr char text6[] = "Runnning";
-static constexpr char text7[] = "ver 0.5";
-static constexpr const char* text[] = { text0, text1, text2, text3, text4, text5, text6, text7 };
-
+void displayStartUp();
 
 void setup() {
     M5.begin();
     M5.Power.begin();
     Serial.begin(9600);
+    //GPS Unit 設定
     //GPSRaw.begin(9600,SERIAL_8N1,21,25);//baudrate, config,rx,tx(ATOM ECHO)
     GPSRaw.begin(9600,SERIAL_8N1,39,38);//baudrate, config,rx,tx(ATOM S3)
-
+    
+    //u8g2設定
+    u8g2.begin(); // start the u8g2 library
     display.begin();
-
     canvas.setColorDepth(1); // mono color
-    canvas.setRotation(180);
     canvas.createSprite(display.width(), display.height());
     canvas.setTextSize((float)canvas.width() / 120);
     canvas.setTextScroll(true);
@@ -39,13 +32,19 @@ void loop() {
     if (GPSRaw.available()){
         int ch = GPSRaw.read();
         Serial.write(ch);
-        canvas.print(ch);
+        //canvas.print(ch);
     }
 
-    static int count = 0;
-    //canvas.printf("%04d:%s\r\n", count, text[count & 7]);
-    canvas.pushSprite(0, 0);
-    ++count;
+    //canvas.pushSprite(0, 0);
+    displayStartUp();
 
     delay(100);
+}
+
+void displayStartUp(){
+    u8g2.clearBuffer();		// clear the internal memory
+    u8g2.setFont(u8g2_font_tenthinguys_tf); // set custom font
+    u8g2.drawStr(25,30,"M5 Scouter"); // print some message
+    u8g2.drawStr(40,45,"Ver 1.01"); // print some message
+    u8g2.sendBuffer();		// transfer internal memory to the display
 }
